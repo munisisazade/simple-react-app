e005f67aa8063fde66cee901dcc7a670484728e9
Munis Isazade <munisisazade@gmail.com>
2017-04-18 18:41:44 +0400

    Simple started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
- 
+import React, {Components} from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const MyApp = () => {
+    return (
+        <div>Simple React app started</div>
+    )
+};
+
+
+ReactDOM.render(
+    <MyApp/>,
+    document.querySelector('.container')
+);
