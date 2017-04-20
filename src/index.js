5b56c6de2a5842ee3a316d7cb0f56a051ccea4dc
Munis Isazade <munisisazade@gmail.com>
2017-04-20 17:20:16 +0400

    Simple dropdown done

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ class MyApp extends Component {
         this.state = {is_open:false,onclassname:"dropdown",item:"Select Language"};
         this.toggle.bind(this);
         this.close.bind(this);
+        this.select.bind(this);
     }
 
     close() {
@@ -36,12 +37,15 @@ class MyApp extends Component {
     toggle() {
         this.setState({is_open:!this.state.is_open});
     }
+    select(text) {
+        this.setState({item:text});
+    }
 
     render() {
         return (
             <div className="App">
                 <h1>React Simple </h1>
-                <DropdownButton term={this.state.item} onClose={() => {this.close()}} onChangeTerm={(item) => {this.setState({item})}} onClass={this.state.is_open} onOpen={() => {this.toggle()}} key={this.handledata()[0].title} data={this.handledata()} />
+                <DropdownButton term={this.state.item} onClose={() => {this.close()}} onChangeTerm={(item) => {this.select(item)}} onClass={this.state.is_open} onOpen={() => {this.toggle()}} key={this.handledata()[0].title} data={this.handledata()} />
             </div>
         );
     }
